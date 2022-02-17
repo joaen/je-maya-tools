65be2291ca62e393953777c4118b2738c8d7d3c4
Joaen <joar.engberg@gmail.com>
2022-02-17 23:57:18 +0100

    Update combine_curves.py

--- rigging/combine_curves.py
+++ rigging/combine_curves.py
@@ -9,3 +9,8 @@ pm.parent(shape_nodes, null_group, s=True, r=True)
 
 pm.delete(selected_curves)
 pm.delete(shape_nodes, constructionHistory=True)
+
+
+pm.circle(normal=(1,0,1), center=(0,0,0))
+pm.circle(normal=(1,0,0), center=(0,0,0))
+pm.circle(normal=(0,0,1), center=(0,0,0))
