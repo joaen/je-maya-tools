1bda0b2ff782a83cfefb90b569937dc1d59b7ca8
Joaen <joar.engberg@gmail.com>
2022-02-18 23:29:47 +0100

    Update combine_curves.py

--- rigging/combine_curves.py
+++ rigging/combine_curves.py
@@ -1,60 +1,39 @@
 import pymel.core as pm
-import maya.cmds as cmds
-
-# out_node = pm.createNode("transform")
-# for axis_normal in [[1,0,0], [0,1,0], [0,0,1]]:
-#     obj = pm.circle(normal=axis_normal)
-#     obj.setTranslation(axis_normal)
-#     circle.setScale()
-#     pm.makeIdentiy(circle, apply=True)
-#     obj.getShape().setParent(out_node, shape=True)
-
 
 def combine_curves(curves):
-    pm.select(clear=True)
-    for i in curves:
-        pm.select(i, add=True)
-    
-    selected_curves = pm.ls(sl=True)
+    selected_curves = curves
     shape_nodes = pm.listRelatives(selected_curves, s=True)
-
-    pm.makeIdentity(selected_curves, apply=True, t=True, r=True, s=True)
     null_group = pm.group(em=True, name="newCtrl")
 
+    pm.makeIdentity(selected_curves, apply=True, t=True, r=True, s=True)
     pm.parent(shape_nodes, null_group, shape=True, relative=True)
-
-    # pm.delete(shape_nodes, constructionHistory=True)
-    # pm.delete(curves)
-    # cmds.delete(selected_curves)
-    # for i in selected_curves:
-    #     pm.delete(i)
-    # print(selected_curves)
+    pm.delete(selected_curves)
+    pm.delete(shape_nodes, constructionHistory=True)
 
 def control_sphere():
-    # 45s
-    circle0 = pm.circle(normal=(1,0,0), center=(0,0,0))
-    pm.rotate(circle0, 0,45,0)
-    
-    circle1 = pm.circle(normal=(1,0,0), center=(0,0,0))
-    pm.rotate(circle1, 0,-45,0)
+    # Create 7 circles that form a sphere
+    circle0 = pm.circle(normal=(1,0,0), center=(0,0,0))[0]
+    circle1 = pm.circle(normal=(1,0,0), center=(0,0,0))[0]
+    circle2 = pm.circle(normal=(1,0,0), center=(0,0,0))[0]
+    circle3 = pm.circle(normal=(0,0,1), center=(0,0,0))[0]
+    circle4 = pm.circle(normal=(0,1,0), center=(0,0,0))[0]
+    circle5 = pm.circle(normal=(0,1,0), center=(0,0,0))[0]
+    circle6 = pm.circle(normal=(0,1,0), center=(0,0,0))[0]
 
-    # straight
-    circle2 = pm.circle(normal=(1,0,0), center=(0,0,0))
-    circle3 = pm.circle(normal=(0,0,1), center=(0,0,0))
+    # Translate, rotate and scale circles
+    move_rotate_scale(circle0, [0,0,0], [0,45,0], [1,1,1])
+    move_rotate_scale(circle1, [0,0,0], [0,-45,0], [1,1,1])
+    move_rotate_scale(circle5, [0,0.7,0], [0,0,0], [0.7,0.7,0.7])
+    move_rotate_scale(circle6, [0,-0.7,0], [0,0,0], [0.7,0.7,0.7])
 
-    # flat 
-    circle4 = pm.circle(normal=(0,1,0), center=(0,0,0))
-    # pm.move(0,0.7,0, circle4)
-
-    circle5 = pm.circle(normal=(0,1,0), center=(0,0,0))
-    pm.move(0,-0.7,0, circle5)
-    pm.scale(circle5, 0.7,0.7,0.7)
-    pm.makeIdentity(circle5, apply=True, translate=True, rotate=True, scale=True)
-
-    circle6 = pm.circle(normal=(0,1,0), center=(0,0,0))
-    pm.move(0,0.7,0, circle6)
-    pm.scale(circle6, 0.7,0.7,0.7)
+    # Combine to one single shape
     combine_curves([circle0, circle1, circle2, circle3, circle4, circle5, circle6])
+
+def move_rotate_scale(obj, tran, rot, scale):
+    obj.setTranslation(tran)
+    obj.setRotation(rot)
+    obj.setScale(scale)
+
 
 control_sphere()
 
