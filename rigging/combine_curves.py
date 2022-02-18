5e8e278905d7aaafb5c05ad31af30e6a4067e638
Joaen <joar.engberg@gmail.com>
2022-02-19 00:04:47 +0100

    Update combine_curves.py

--- rigging/combine_curves.py
+++ rigging/combine_curves.py
@@ -11,23 +11,22 @@ def combine_curves(curves):
     pm.delete(shape_nodes, constructionHistory=True)
 
 def control_sphere():
-    # Create 7 circles that form a sphere
-    circle0 = pm.circle(normal=(1,0,0), center=(0,0,0))[0]
-    circle1 = pm.circle(normal=(1,0,0), center=(0,0,0))[0]
-    circle2 = pm.circle(normal=(1,0,0), center=(0,0,0))[0]
-    circle3 = pm.circle(normal=(0,0,1), center=(0,0,0))[0]
-    circle4 = pm.circle(normal=(0,1,0), center=(0,0,0))[0]
-    circle5 = pm.circle(normal=(0,1,0), center=(0,0,0))[0]
-    circle6 = pm.circle(normal=(0,1,0), center=(0,0,0))[0]
+    # Create circles and add to list
+    circles = []
+    for n in range(0, 7):
+        circles.append(pm.circle(normal=(0,0,0), center=(0,0,0))[0])
 
     # Translate, rotate and scale circles
-    move_rotate_scale(circle0, [0,0,0], [0,45,0], [1,1,1])
-    move_rotate_scale(circle1, [0,0,0], [0,-45,0], [1,1,1])
-    move_rotate_scale(circle5, [0,0.7,0], [0,0,0], [0.7,0.7,0.7])
-    move_rotate_scale(circle6, [0,-0.7,0], [0,0,0], [0.7,0.7,0.7])
-
+    move_rotate_scale(circles[0], [0,0,0], [0,45,0], [1,1,1])
+    move_rotate_scale(circles[1], [0,0,0], [0,-45,0], [1,1,1])
+    move_rotate_scale(circles[2], [0,0,0], [0,0,0], [1,1,1])
+    move_rotate_scale(circles[3], [0,0,0], [0,-90,0], [1,1,1])
+    move_rotate_scale(circles[4], [0,0,0], [90,0,0], [1,1,1])
+    move_rotate_scale(circles[5], [0,0.7,0], [90,0,0], [0.7,0.7,0.7])
+    move_rotate_scale(circles[6], [0,-0.7,0], [90,0,0], [0.7,0.7,0.7])
+    
     # Combine to one single shape
-    combine_curves([circle0, circle1, circle2, circle3, circle4, circle5, circle6])
+    combine_curves(circles)
 
 def move_rotate_scale(obj, tran, rot, scale):
     obj.setTranslation(tran)
