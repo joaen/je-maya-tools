37903775c7400d9de599c0c2660a90d6b0e1784b
Joaen <joar.engberg@gmail.com>
2022-02-20 23:56:47 +0100

    update

--- rigging/ctrl_creator.py
+++ rigging/ctrl_creator.py
@@ -8,7 +8,7 @@ def get_cvs(object):
 
 def create_controller():
     for transform in pm.selected():
-        shape = get_cube()
+        shape = get_sphere()
         pm.rename(shape, transform+"_CTRL")
         offset_grp = pm.group(shape)
         pm.rename(offset_grp, transform+"_CTRL_Grp")
@@ -37,32 +37,25 @@ def rotate_ctrl_shape(degrees):
     for sel in stored_selection:
         pm.select(sel, add=True)
 
-def combine_curves(curves):
-    shape_nodes = pm.listRelatives(curves, s=True)
-    output_node = pm.group(em=True, name="newCtrl")
-    pm.makeIdentity(curves, apply=True, t=True, r=True, s=True)
-    pm.parent(shape_nodes, output_node, shape=True, relative=True)
-    pm.delete(curves)
-    pm.delete(shape_nodes, constructionHistory=True)
-    return output_node
-
 def get_sphere():
     # Create circles and add to list
     circles = []
-    for n in range(0, 7):
+    for n in range(0, 3):
         circles.append(pm.circle(normal=(0,0,0), center=(0,0,0))[0])
 
-    # Translate, rotate and scale circles
-    move_rotate_scale(circles[0], [0,0,0], [0,45,0], [1,1,1])
-    move_rotate_scale(circles[1], [0,0,0], [0,-45,0], [1,1,1])
-    move_rotate_scale(circles[2], [0,0,0], [0,0,0], [1,1,1])
-    move_rotate_scale(circles[3], [0,0,0], [0,-90,0], [1,1,1])
-    move_rotate_scale(circles[4], [0,0,0], [90,0,0], [1,1,1])
-    move_rotate_scale(circles[5], [0,0.7,0], [90,0,0], [0.7,0.7,0.7])
-    move_rotate_scale(circles[6], [0,-0.7,0], [90,0,0], [0.7,0.7,0.7])
+    move_rotate_scale(circles[0], [0,0,0], [0,0,0], [1,1,1])
+    move_rotate_scale(circles[1], [0,0,0], [0,-90,0], [1,1,1])
+    move_rotate_scale(circles[2], [0,0,0], [90,0,0], [1,1,1])
     
-    # Combine to one single shape
-    new_sphere = combine_curves(circles)
+    # Combines circles
+    shape_nodes = pm.listRelatives(circles, s=True)
+    output_node = pm.group(em=True, name="newCtrl")
+    pm.makeIdentity(circles, apply=True, t=True, r=True, s=True)
+    pm.parent(shape_nodes, output_node, shape=True, relative=True)
+    pm.delete(circles)
+    pm.delete(shape_nodes, constructionHistory=True)
+    
+    new_sphere = output_node
     return new_sphere
 
 
@@ -75,24 +68,9 @@ def get_circle():
     return pm.circle(normal=(1, 0, 0), center=(0, 0, 0))
 
 def get_cube():
-    top_square = get_square([0,1,0])
-    top_square.setTranslation([0,0.5,0])
-    bottom_square = get_square([0,1,0])
-    bottom_square.setTranslation([0,-0.5,0])
+    return pm.curve(name="shape123", d=1, p=[(-1, -1, 1), (-1, 1, 1), (1, 1, 1), (1, -1, 1), (-1, -1, 1), (-1, -1, -1), (-1, 1, -1), (-1, 1, 1), (-1, 1, -1), (1, 1, -1), (1, -1, -1), (-1, -1, -1), (1, -1, -1), (1, -1, 1), (1, 1, 1), (1, 1, -1)], k=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
 
-    c1= pm.curve(degree=1, p=[(0.5, 0.5, 0.5), (0.5, -0.5, 0.5)], k=[0,1])
-    c2 = pm.curve(degree=1, p=[(0.5, 0.5, -0.5), (0.5, -0.5, -0.5)], k=[0,1])
-    c3 = pm.curve(degree=1, p=[(-0.5, 0.5, -0.5), (-0.5, -0.5, -0.5)], k=[0,1])
-    c4 = pm.curve(degree=1, p=[(-0.5, 0.5, 0.5), (-0.5, -0.5, 0.5)], k=[0,1])
-
-    new_cube = combine_curves([top_square, bottom_square, c1, c2, c3, c4])
-    return new_cube
-
-def get_square(normal_axis):
-    square = pm.nurbsSquare(normal=(normal_axis), center=(0, 0, 0))
-    curves = pm.listRelatives(square, children=True)
-    new_square = combine_curves(curves)
-    pm.delete(square[0])
-    return new_square
+def get_square():
+    return pm.curve(d=1, p=[(-1, 0, 1), (-1, 0, 1), (0, 0, 1), (1, 0, 1), (1, 0, 1), (1, 0, -1), (-1, 0, -1), (-1, 0, 1)], k=[0,1,2,3,4,5,6,7])
 
 create_controller()
