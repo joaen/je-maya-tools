8965710e6d4bd54b7d3e42b2a08cb54263116e92
Joaen <joar.engberg@gmail.com>
2022-02-28 00:21:01 +0100

    Update calculate_pole_vector.py

--- rigging/calculate_pole_vector.py
+++ rigging/calculate_pole_vector.py
@@ -8,37 +8,41 @@ Add calculate_pole_vector.py to your Maya scripts folder (Username\Documents\may
 
 To calculate the pole position and create a locator at that position you can select three joints in Maya (in top-down order) and run this command or add it to a shelf button:
 
-from calculate_pole_vector import create_loc
-create_loc(offset="2")
+import calculate_pole_vector
+calculate_pole_vector.create_loc(offset=2)
+
+If you just want query the correct pole vector position you can run this commands and use your preffered transforms as input:
+
+import calculate_pole_vector
+calculate_pole_vector.get_position("pSphere1", "pSphere2", "pSphere1", offset=2)
 
 '''
-import pymel.core as pm
+import maya.cmds as cmds
+import maya.api.OpenMaya as om
 
 
 def get_position(start_joint, mid_joint, end_joint, offset):
-    # Create pynodes
-    joint1_node = pm.PyNode(start_joint)
-    joint2_node = pm.PyNode(mid_joint)
-    joint3_node = pm.PyNode(end_joint)
+    # Get joint poistions as vectors
+    joint1_pos = om.MVector(cmds.xform(start_joint, query=True, worldSpace=True, translation=True))
+    joint2_pos = om.MVector(cmds.xform(mid_joint, query=True, worldSpace=True, translation=True))
+    joint3_pos = om.MVector(cmds.xform(end_joint, query=True, worldSpace=True, translation=True))
 
-    # Get joint poistions
-    joint1_pos = joint1_node.getTranslation(space="world")
-    joint2_pos = joint2_node.getTranslation(space="world")
-    joint3_pos = joint3_node.getTranslation(space="world")
-    
     # Calculate the mid point between joint1 and joint3
     mid_point_pos = joint1_pos + (joint3_pos - joint1_pos).normal() * ((joint3_pos - joint1_pos).length() * 0.5)
 
     # Get the pole vector position by aiming from the mid point towards joint2 position. Scale the vector using the offset float.
     pole_vec = mid_point_pos + (joint2_pos - mid_point_pos).normal() * ((joint2_pos - mid_point_pos).length() * offset)
-    print("Pole position: "+str(pole_vec))
     
+    print("Pole position: "+str(pole_vec))
     return pole_vec
 
 def create_loc(offset=2):
-    joint1 = pm.selected()[0]
-    joint2 = pm.selected()[1]
-    joint3 = pm.selected()[2]
+    joint1 = cmds.ls(selection=True)[0]
+    joint2 = cmds.ls(selection=True)[1]
+    joint3 = cmds.ls(selection=True)[2]
     pole_vector_pos = get_position(joint1, joint2, joint3, offset)
-    pm.spaceLocator().setTranslation(pole_vector_pos, space="world")
+
+    loc = cmds.spaceLocator()
+    cmds.move(pole_vector_pos[0], pole_vector_pos[1], pole_vector_pos[2], loc, absolute=True, worldSpace=True)
+
 
