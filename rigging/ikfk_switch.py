412396a42c579780993453ec4d3446062da82ead
Joaen <joar.engberg@gmail.com>
2022-03-03 20:13:16 +0100

    Update ikfk_switch.py

--- rigging/ikfk_switch.py
+++ rigging/ikfk_switch.py
@@ -30,7 +30,6 @@ class TemplateToolWindow(QtWidgets.QDialog):
     def create_ui_widgets(self):
         self.template_label = QtWidgets.QLabel("Switch IK/FK:")
 
-        # self.t
         self.template_button1 = QtWidgets.QPushButton("RIGHT ARM")
         self.template_button1.setStyleSheet("background-color: lightgreen; color: black")
         self.template_button2 = QtWidgets.QPushButton("LEFT ARM")
@@ -49,39 +48,23 @@ class TemplateToolWindow(QtWidgets.QDialog):
         self.close_button = QtWidgets.QPushButton("Close")
         self.close_button.setFixedWidth(100)
 
+        # setting background color to push button when mouse hover over it
+        icon_button_css = (
+            "QPushButton { background-color: #444444; border: transparent; border-radius: 12px; }"
+            "QPushButton:hover { background-color: #5D5D5D; }"
+            "QPushButton:pressed {background-color: black; }")
+        
         self.template_icon_button = QtWidgets.QPushButton()
         self.template_icon_button.setIcon(QtGui.QIcon(":advancedSettings.png"))
         self.template_icon_button.setFixedSize(24, 24)
-        self.template_icon_button.setStyleSheet("background-color: #444444; border: 1px solid #444444;")
+        self.template_icon_button.setStyleSheet(icon_button_css)
 
         self.help_icon_button = QtWidgets.QPushButton()
         self.help_icon_button.setIcon(QtGui.QIcon(":help.png"))
         self.help_icon_button.setFixedSize(24, 24)
-        self.help_icon_button.setStyleSheet("background-color: #444444; border: 1px solid #444444;")
+        self.help_icon_button.setStyleSheet(icon_button_css)
 
-         # setting background color to push button when mouse hover over it
-        self.template_icon_button.setStyleSheet("QPushButton {"
-                            "background-color: #444444;"
-                            "border: 1px solid #444444;"
-                            "border-radius: 12px;"
-                            "}"
-                            "QPushButton:hover {"
-                            "background-color: #5D5D5D;"
-                            "}")
-
-        self.help_icon_button.setStyleSheet("QPushButton {"
-                            "background-color: #444444;"
-                            "border: 1px solid #444444;"
-                            "border-radius: 12px;"
-                            "}"
-                            "QPushButton:hover {"
-                            "background-color: #5D5D5D;"
-                            "}")
-
-            
-  
-
-
+        
 
     def create_ui_layout(self):
 
@@ -116,12 +99,67 @@ class TemplateToolWindow(QtWidgets.QDialog):
         # main_layout.addLayout(vertical_layout)
  
     def create_ui_connections(self):
-        self.template_button1.clicked.connect(self.template_command)
-        self.template_icon_button.clicked.connect(self.template_command)
-        self.template_slider.valueChanged.connect(self.template_command)
+        self.template_button1.clicked.connect(self.open_settings_window)
+        self.template_icon_button.clicked.connect(self.open_settings_window)
+        self.template_slider.valueChanged.connect(self.open_settings_window)
+
+    def open_settings_window(self):
+        try:
+            self.settings_window.close()
+            self.settings_window.deleteLater()
+        except:
+            pass
+        self.settings_window = SettingsWindow()
+        self.settings_window.show()
+
+
+class SettingsWindow(QtWidgets.QDialog):
+    textfield_label_list = [
+        "ikfk_attr_name", "fk_ctrl_start", "fk_ctrl_mid", "fk_ctrl_end",
+        "ik_target_start", "ik_target_mid", "ik_target_end", "ik_ctrl",
+        "ik_pv_ctrl", "fk_target_start", "fk_target_mid", "fk_target_end"
+    ]
+
+
+    def __init__(self):
+        super(SettingsWindow, self).__init__(maya_main_window())
+        self.setWindowTitle("Settings")
+        
+        self.setWindowFlags(self.windowFlags() ^ QtCore.Qt.WindowContextHelpButtonHint)
+        self.resize(320, 120)
+        self.create_ui_widgets()
+        self.create_ui_layout()
+        self.create_ui_connections()
+
+        if cmds.about(macOS=True):
+            self.setWindowFlags(QtCore.Qt.Tool)
+ 
+    def create_ui_widgets(self):
+        self.textfield_widget_list = []
+        self.label_widget_list = []
+        for i in self.textfield_label_list:
+            label = QtWidgets.QLabel(str(i))
+            textfield = QtWidgets.QLineEdit()
+            self.textfield_widget_list.append(textfield)
+            self.label_widget_list.append(label)
+            
+    def create_ui_layout(self):
+        main_layout = QtWidgets.QVBoxLayout(self)
+        main_layout.setContentsMargins(10, 20, 10, 10)
+
+        for n in range(0, 12):
+            label_layout = QtWidgets.QHBoxLayout()
+            label_layout.addWidget(self.label_widget_list[n])
+            label_layout.addWidget(self.textfield_widget_list[n])
+            main_layout.addLayout(label_layout)
+
+        main_layout.addStretch()
+ 
+    def create_ui_connections(self):
+        print("WOW")
 
     def template_command(self):
-        print("TEMPLATE!")
+        print("O")
 
 def start():
     global template_tool_ui
