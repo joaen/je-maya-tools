1f86463ca79ce76d439fbecfc30e73a196ac21cd
Joaen <joar.engberg@gmail.com>
2022-03-29 15:40:05 +0200

    Update ribbon_spine.py

--- rigging/ribbon_spine.py
+++ rigging/ribbon_spine.py
@@ -1,65 +1,18 @@
 import maya.mel as mel
 import maya.cmds as cmds
 
-# 
-
-def create_ribbonspone():
-    joints = []
-    dupes = []
-
-    mel.eval("nurbsPlane -p 0 0 0 -ax 0 0 1 -w 1 -lr 50 -d 3 -u 1 -v 5 -ch 1; objectMoveCommand;")  
-    mel.eval("rebuildSurface -ch 1 -rpo 1 -rt 0 -end 1 -kr 0 -kcp 0 -kc 1 -su 1 -du 1 -sv 5 -dv 3 -tol 0.01 -fr 0 -dir 0;")
-    mel.eval("createHair 1 5 10 0 0 0 0 5 0 2 1 1;")
-
-    curves = cmds.listRelatives("hairSystem1OutputCurves", children=True)
-
-    cmds.select(clear=True)
-    for i in curves:
-        cmds.select(i, add=True)
-    cmds.select("hairSystem1OutputCurves", add=True)
-    cmds.select("hairSystem1", add=True)
-    cmds.select("nucleus1", add=True)
-    cmds.delete()
-
-    folliclesList = cmds.listRelatives("hairSystem1Follicles", children=True)
-    for i in folliclesList:
-        jnt = cmds.joint(p=(0, 0, 0))
-
-        joints.append(jnt)
-
-        cmds.parent(jnt, i)
-        cmds.xform(jnt, t=(0, 0, 0) )
-
-        dupe = cmds.duplicate(jnt)[0]
-        dupes.append(dupe)
-        cmds.parent(dupe, world=True)
-
-    cmds.skinCluster(dupes, "nurbsPlane1")
-
-    for i in dupes:
-        cmds.rename(i, "BindToPlane#")
-
-    for i in joints:
-        cmds.rename(i, "BindToGeo#")
-
 def create_ribbon():
     selection = cmds.ls(selection=True)
     point_list = []
     joints = []
     dupes = []
 
+    # Add joint positions to list
     for j in selection:
         joint_pos = cmds.xform(j, query=True, translation=True, worldSpace=True)
         point_list.append(joint_pos)
-    
-    # point_list 
-    # point_list.append((0,0,0))
-    # point_list.append((0,1,0))
-    # ikHandle -sol ikSplineSolver -scv false -roc false -pcv false;
-    
 
     # Create plane
-    # curve = cmds.curve(degree=3, point=point_list)
     temp_ik_handle = cmds.ikHandle(startJoint=selection[0], endEffector=selection[len(selection) - 1], sol="ikSplineSolver", simplifyCurve=False, rootOnCurve=False, parentCurve=False)
     cmds.delete(temp_ik_handle[0], temp_ik_handle[1])
     curve = temp_ik_handle[2]
@@ -70,33 +23,35 @@ def create_ribbon():
     plane = cmds.loft(curve_dupe, curve, constructionHistory=False, range=False, autoReverse=True)
     cmds.delete(curve, curve_dupe)
 
-    # Create hair
+    # Create follicles
     cmds.select(plane, replace=True)
-    mel.eval("createHair 1 4 10 0 0 1 0 5 0 2 1 1;")  
+    mel.eval("createHair 1 {} 10 0 0 1 0 5 0 2 1 1;".format(len(selection)))  
     cmds.delete("hairSystem1OutputCurves", "hairSystem1", "nucleus1")
 
     # Create joints
-    folliclesList = cmds.listRelatives("hairSystem1Follicles", children=True)
-    for i in folliclesList:
+    follicle_list = cmds.listRelatives("hairSystem1Follicles", children=True)
+
+    for i in range(len(follicle_list)):
         jnt = cmds.joint(p=(0, 0, 0))
 
         joints.append(jnt)
 
-        cmds.parent(jnt, i)
+        cmds.parent(jnt, follicle_list[i])
         cmds.xform(jnt, t=(0, 0, 0) )
 
         dupe = cmds.duplicate(jnt)[0]
         dupes.append(dupe)
         cmds.parent(dupe, world=True)
+        cmds.parentConstraint(jnt, selection[i], maintainOffset=True)
 
+    # Skin joints to plane
     cmds.skinCluster(dupes, plane)
 
     for i in dupes:
-        cmds.rename(i, "BindToPlane#")
+        cmds.rename(i, "spine_control_jnt#")
 
     for i in joints:
-        cmds.rename(i, "BindToGeo#")
-
+        cmds.rename(i, "geometry_control_jnt#")
 
 
 create_ribbon()
